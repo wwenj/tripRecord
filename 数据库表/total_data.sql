3c02caaa48956fce1f6e09ffaa6380af9e4874de
wangwenjian <man@wwenj.com>
2019-06-10 12:00:03 +0800

    数据库表

---
+++ 数据库表/total_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `total_data` VALUES (1, '85.99', '46.59', '81.40', '40.5', '68.54', '44.78');
+INSERT INTO `total_data` VALUES (2, '10.3', '40.7', '17.9', '33.5', '5.8', '12.4');
+INSERT INTO `total_data` VALUES (3, '12.4', '44.9', '32.0', '47.2', '12.4', '22.9');
